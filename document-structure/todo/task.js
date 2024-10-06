c3fcc22532d40777be8d3f85f26f622aa3402f47
Boo <fashion.backpack@yandex.ru>
2024-10-07 01:22:28 +0300

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,23 @@
+let form = document.querySelector('form')
+let input = document.querySelector('.tasks__input')
+let list = document.querySelector('.tasks__list')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value.trim().length != 0) {
+    list.insertAdjacentHTML('afterbegin', `
+    <div class="task">
+    <div class="task__title">${input.value}</div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`);
+    let tasks = document.querySelectorAll('.task')
+
+    tasks.forEach((step) => {
+      step.lastElementChild.addEventListener('click', (e) => {
+        e.preventDefault();
+        step.remove();
+      })
+    })
+    form.reset()
+  }
+})
